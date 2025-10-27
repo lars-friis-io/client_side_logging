f46c6323587dd015a7f0ef7a60411040bda02c0c
Lars Friis <lars@larsfriis.io>
2025-10-27 09:19:50 +0100

    Update sendLog.js

--- sendLog.js
+++ sendLog.js
@@ -1,27 +1,22 @@
 (function () {
   window.larsFriis = window.larsFriis || {};
 
-  window.larsFriis.sendLog = function (datalayer) {
-    const page_location = window.location.href;
-    const user_agent = navigator.userAgent;
-    const device_type = /Mobi|Android|iPhone|iPad|iPod/i.test(user_agent)
-      ? "mobile"
-      : "desktop";
-    const source = "client";
-
-    // Hent event fra datalayer eller fallback
-    const event =
-      datalayer && typeof datalayer === "object" && datalayer.event
-        ? datalayer.event
-        : "message";
+  window.larsFriis.sendLog = function (containerId) {
+    const dataModel = window.google_tag_manager[containerId].dataLayer.get({
+      split: function () {
+        return [];
+      }
+    });
 
     const trackingData = {
-      page_location,
-      user_agent,
-      device_type,
-      source,
-      event,
-      datalayer, // hele input-objektet
+      page_location: window.location.href,
+      user_agent: navigator.userAgent,
+      device_type: /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)
+        ? "mobile"
+        : "desktop",
+      source: "client",
+      event: dataModel.event,
+      datalayer: dataModel
     };
 
     console.log("Client tracking data:", trackingData);
